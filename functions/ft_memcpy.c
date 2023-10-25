b954cbe392154d51f04e278cf4603d38514043d3
Armin Kuburas <akuburass@gmail.com>
2023-10-25 16:33:51 +0300

    fixed minor mistake

--- functions/ft_memcpy.c
+++ functions/ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 16:26:20 by akuburas          #+#    #+#             */
-/*   Updated: 2023/10/25 16:32:01 by akuburas         ###   ########.fr       */
+/*   Updated: 2023/10/25 16:33:24 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t = i;
+	size_t	i;
 
 	i = 0;
 	while (i < n)
