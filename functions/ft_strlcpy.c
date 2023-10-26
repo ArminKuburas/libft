209ca161d5ae5bcb9c344fc08af7bcad624fe4c0
Armin Kuburas <akuburass@gmail.com>
2023-10-26 11:58:56 +0300

    Minor fixes and made strlcpy clone

---
+++ functions/ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 10:19:57 by akuburas          #+#    #+#             */
+/*   Updated: 2023/10/26 10:57:48 by akuburas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	src_len;
+	size_t	i;
+
+	src_len = 0;
+	i = 0;
+	while (src[src_len] != '\0')
+	{
+		src_len++;
+	}
+	if (dstsize > 0)
+	{
+		while (i < src_len && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (src_len);
+}
