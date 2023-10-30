aaa6d75c42efc6bbce7b653810f7cf33828d730f
Armin Kuburas <akuburass@gmail.com>
2023-10-30 14:13:30 +0200

    Made some fixes to ft_split. Should be better now?

--- libft_proper/libft/ft_split.c
+++ libft_proper/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 09:15:30 by akuburas          #+#    #+#             */
-/*   Updated: 2023/10/30 12:38:35 by akuburas         ###   ########.fr       */
+/*   Updated: 2023/10/30 14:12:15 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ static int	count_substrings(const char *str, char c)
 			in_substring = 0;
 		str++;
 	}
-
 	return (count);
 }
 
@@ -49,30 +48,59 @@ static char	*substring_create(const char *str, int start, int finish)
 	return (substr);
 }
 
-char	**ft_split(char const *s, char c)
+static void	free_result_array(char **result, int j)
+{
+	while (j > 0)
+	{
+		free(result[j--]);
+	}
+	free(result);
+}
+
+static void	split_string(char const *s, char c, char **result, int count)
 {
 	size_t	i;
 	int		j;
 	int		index;
-	char	**result;
 
-	result = (char **)malloc((count_substrings(s, c) + 1) * sizeof(char *));
-	if (!s || !result)
-		return (NULL);
 	i = 0;
 	j = 0;
 	index = -1;
-	while (j < count_substrings(s, c))
+	while (j < count)
 	{
 		if (s[i] != c && index < 0)
 			index = i;
 		else if ((s[i] == c || !s[i]) && index >= 0)
 		{
-			result[j++] = substring_create(s, index, i);
+			result[j] = substring_create(s, index, i);
+			if (!result[j])
+			{
+				free_result_array(result, j);
+				return (NULL);
+			}
 			index = -1;
+			j++;
 		}
 		i++;
 	}
-	result[j] = (NULL);
+	result[j] = NULL;
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		count;
+	char	**result;
+
+	result = (char **)malloc((count_substrings(s, c) + 1) * sizeof(char *));
+	if (!s || !result)
+		return (NULL);
+	count = count_substrings(s, c);
+	split_string(s, c, result, count);
+	return (result);
+	if (!result[count])
+	{
+		free(result);
+		return (NULL);
+	}
 	return (result);
 }
