9ce6a2bddfafd9c9f002ff1627ae957e30674e82
Armin Kuburas <akuburass@gmail.com>
2023-10-31 09:08:55 +0200

    Added putstr_fd. updated header and makefile

--- libft_proper/libft/libft.h
+++ libft_proper/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 07:40:02 by akuburas          #+#    #+#             */
-/*   Updated: 2023/10/31 08:51:15 by akuburas         ###   ########.fr       */
+/*   Updated: 2023/10/31 09:06:56 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,6 @@ char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 #endif
